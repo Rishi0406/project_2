c9f74ee2cd00dae2af0318095b8b43165ca401d6
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-09-24 11:35:36 +1000

    updated schema

--- Data/Parking.sql
+++ Data/Parking.sql
@@ -1,10 +1,6 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "Parking_bay" (
     "bay_id" integer   NOT NULL,
-    "area_id" integer   NOT NULL,
+    "areaname_id" integer   NOT NULL,
     "lat" float   NOT NULL,
     "lon" float   NOT NULL,
     CONSTRAINT "pk_Parking_bay" PRIMARY KEY (
@@ -13,9 +9,9 @@ CREATE TABLE "Parking_bay" (
 );
 
 CREATE TABLE "Parking_duration" (
-    "arrival_time" date   NOT NULL,
-    "departure_time" date   NOT NULL,
-    "duration_minutes" integer   NOT NULL,
+    "arrivaltime" date   NOT NULL,
+    "departuretime" date   NOT NULL,
+    "durationminutes" integer   NOT NULL,
     "bay_id" integer   NOT NULL
 );
 
@@ -27,7 +23,7 @@ CREATE TABLE "Area" (
      )
 );
 
-ALTER TABLE "Parking_bay" ADD CONSTRAINT "fk_Parking_bay_area_id" FOREIGN KEY("area_id")
+ALTER TABLE "Parking_bay" ADD CONSTRAINT "fk_Parking_bay_areaname_id" FOREIGN KEY("areaname_id")
 REFERENCES "Area" ("area_id");
 
 ALTER TABLE "Parking_duration" ADD CONSTRAINT "fk_Parking_duration_bay_id" FOREIGN KEY("bay_id")
